1dcfee56f579ea4d095701c9a52ff3b5ece23e44
_FN10_ <mathdudex09@gmail.com>
2025-12-03 20:07:15 -0400

    finish download section on index

--- latest-loader.js
+++ latest-loader.js
@@ -1,2 +1,16 @@
-var converter = new showdown.Converter()
+var converter = new showdown.Converter();
 
+var latestVersionTag = document.getElementById("latest-version-tag"),
+    latestVersionBody = document.getElementById("latest-markdown-body");
+
+fetch("https://api.github.com/repos/xFN10x/bedrockR/releases/latest")
+    .then((res) => {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then((json) => {
+        console.log(json);
+        latestVersionTag.innerText = json.tag_name;
+        latestVersionBody.insertAdjacentHTML("afterbegin", converter.makeHtml(json.body))
+    });
